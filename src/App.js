d1a1d81b3ee24e7b55f8daa2da1540296503a070
Katusha-K <119417500+Katusha-K@users.noreply.github.com>
2024-01-01 17:57:08 +0100

    Excluded `Learn React` link from App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <h1>Hi there!</h1>
         <Greeting city="Rome" />
         <Happiness />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
